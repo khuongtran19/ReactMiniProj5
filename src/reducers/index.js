629f7ab2c95775ca9ae34fc8e6561616f6072d38
khuong <tranvinhkhuong@gmail.com>
2020-06-25 14:27:11 -0700

    update

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+const songReducer = () => {
+    return [
+        {
+            title: 'more and more',
+            duration: '4:01'
+        },
+        {
+            title: 'I want it that way',
+            duration: '1:45'
+        },
+        {
+            title: 'All star',
+            duration: '2:39'
+        }
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectedSong
+}
